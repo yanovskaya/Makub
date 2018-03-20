8ff256b8ec5cb48bce891d70ba54d7efd6020fa2
yanovskaya <yanovskaya1997@list.ru>
2018-03-21 01:22:21 +0300

    разные точки входа в приложение

--- Makub/Classes/Presentation/Auth/ViewController/RecoverViewController.swift
+++ Makub/Classes/Presentation/Auth/ViewController/RecoverViewController.swift
@@ -158,7 +158,7 @@ final class RecoverViewController: UIViewController {
     
     private func isValidEmail(_ testStr: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: testStr)
     }
     
--- Makub/Classes/Presentation/Common/TabBarController.swift
+++ Makub/Classes/Presentation/Common/TabBarController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 final class TabBarController: UITabBarController {
     
-    // MARK: - Private Properties
+    // MARK: - Constants
     
+    private enum Constants {
+        static var newsImage = "newspaper"
+        static var ratingImage = "star"
+    }
     private let newsStoryboard = UIStoryboard(with: StoryboardTitle.news)
     private let ratingStoryboard = UIStoryboard(with: StoryboardTitle.rating)
 
@@ -20,13 +24,17 @@ final class TabBarController: UITabBarController {
 
         createTabBarController()
     }
+
     
     func createTabBarController() {
-        let firstVc = newsStoryboard.viewController(NewsViewController.self)
-        firstVc.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 0)
+        let im = UIImage(named: Constants.newsImage)?.imageWithInsets(insets: UIEdgeInsetsMake(6, 0, -6, 0))
         
+        let firstVc = newsStoryboard.viewController(NewsViewController.self)
+        firstVc.tabBarItem = UITabBarItem(title: nil, image: im, tag: 0)
+        
+        var im2 = UIImage(named: Constants.ratingImage)?.imageWithInsets(insets: UIEdgeInsetsMake(4, 0, -4, 0))
         let secondVc = ratingStoryboard.viewController(RatingViewController.self)
-        secondVc.tabBarItem = UITabBarItem(tabBarSystemItem: .downloads, tag: 1)
+        secondVc.tabBarItem = UITabBarItem(title: nil, image: im2, tag: 1)
         
         let controllerArray = [firstVc, secondVc]
         viewControllers = controllerArray.map { UINavigationController(rootViewController: $0) }
