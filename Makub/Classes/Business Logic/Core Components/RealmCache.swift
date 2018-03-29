9fd89d87cb1709c600cccc5c905039725a3e4da4
yanovskaya <yanovskaya1997@list.ru>
2018-03-29 17:51:44 +0300

    мелкие правки

--- Makub/Classes/Business Logic/Core Components/RealmCache.swift
+++ Makub/Classes/Business Logic/Core Components/RealmCache.swift
@@ -20,7 +20,6 @@ final class RealmCache<T> where T: Object {
     // MARK: - Public Methods
     
     func getCachedObject() -> T? {
-        print("get")
         guard let realm = realm, !realm.objects(T.self).isEmpty else { return nil }
         return Array(realm.objects(T.self)).first
     }
@@ -32,7 +31,6 @@ final class RealmCache<T> where T: Object {
     
     func refreshCache(_ object: T) {
         do {
-            print("refresh")
             try? realm?.write {
                 cleanCache()
                 realm?.add(object)
@@ -54,7 +52,6 @@ final class RealmCache<T> where T: Object {
     // MARK: - Private Methods
     
     private func cleanCache() {
-        print("clean")
         guard let realm = realm, !realm.objects(T.self).isEmpty else { return }
         let objects = realm.objects(T.self)
         realm.delete(objects)
