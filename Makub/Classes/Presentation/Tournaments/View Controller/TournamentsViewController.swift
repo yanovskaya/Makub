5cbace0ab07586725963931a1891910fdce1256b
yanovskaya <yanovskaya1997@list.ru>
2018-04-17 16:29:12 +0300

    подготовка нового экрана для турниров

---
+++ Makub/Classes/Presentation/Tournaments/View Controller/TournamentsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TournamentsViewController.swift
+//  Makub
+//
+//  Created by Елена Яновская on 17.04.2018.
+//  Copyright © 2018 Elena Yanovskaya. All rights reserved.
+//
+
+import UIKit
+
+class TournamentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
